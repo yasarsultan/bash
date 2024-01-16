7b5cf2ec7a07eed2a76fab882062f7d14ef5dac7
Yasar Sultan <92508921+yasarsultan@users.noreply.github.com>
2024-01-16 21:32:02 +0000

    committed to bash

---
+++ day1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "I don't have to be great to start, but I have to start to be great!"
---
+++ day2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SKILL="shell scripting"
+echo "I want to be good at ${SKILL}. That's why I'm practicing ${SKILL}."
+
